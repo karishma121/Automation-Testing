e2fd992f55ca06b7d59a8a2e9a0b2b2095b92926
Karishma Limkar <141804139+karishma121@users.noreply.github.com>
2023-08-24 11:24:46 +0530

    Add files via upload

--- TABEvent.java
+++ TABEvent.java
@@ -5,24 +5,19 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
-public class task {
-
+public class TABEvent {
 	public static void main(String[]args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
-		WebElement email = driver.findElement(By.id("email"));
-		//WebElement password = driver.findElement(By.id("pass"));
+		WebElement login = driver.findElement(By.id("email"));
+		login.sendKeys("Karishma");
 		
 		Actions act = new Actions(driver);
-		Action xyz = act.moveToElement(email).click().sendKeys("Karishma")
-		.doubleClick(email).
-		keyDown(email,Keys.TAB).sendKeys("1234").keyDown(Keys.ENTER).build();
-		xyz.perform();
+		act.keyDown(login,Keys.TAB).sendKeys("1234").perform();
 		
-}
-}
+	}
 
+}
